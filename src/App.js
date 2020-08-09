68f1c975c1f3328e65cb175d334073cdb73b54d2
Sujay Poojari <59353836+sujaypoojari@users.noreply.github.com>
2020-08-09 19:16:30 +0530

    removed unnessary file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    languages: [
+      { name: "Go", votes: 0, color: "#008CBA" },
+      { name: "Python", votes: 0, color: "#f44336" },
+      { name: "Dart", votes: 0, color: "#e7e7e7" },
+      { name: "Java", votes: 0, color: "#008CBA" },
+    ],
+  };
+
+  vote(index) {
+    let newLanguages = [...this.state.languages];
+    newLanguages[index].votes++;
+    this.setState({ languages: newLanguages });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Vote Your Language</h1>
+        <div className="languages">
+          {this.state.languages.map((lang, i) => (
+            <div key={i} className="language">
+              <div className="voteCount">{lang.votes}</div>
+              <div className="languageName">{lang.name}</div>
+              <button onClick={this.vote.bind(this, i)}>Vote</button>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
